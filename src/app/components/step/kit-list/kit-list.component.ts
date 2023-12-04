c13bfd0df046625b437ae010c82fce1325835dab
lucas silva <lucas.goliveirass@gmail.com>
2023-12-04 15:27:29 -0300

    CSS e responsividade do cadastro de kit finalizado

--- src/app/components/step/kit-list/kit-list.component.ts
+++ src/app/components/step/kit-list/kit-list.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { KitService } from 'src/app/services/kit.service';
+import { Kit } from 'src/app/interfaces/Kit';
+import { environment } from 'src/environments/environment';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-kit-list',
@@ -6,5 +10,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./kit-list.component.css']
 })
 export class KitListComponent {
+  allKits: Kit[] = []
+  kits: Kit[] = []
+  baseApiUrl = environment.baseApiUrl
 
+  faSearch = faSearch
+  searchTerm: string = ''; 
+
+  constructor(private kitService: KitService) {}
+
+  ngOnInit(): void {
+    this.kitService.getKits().subscribe((items) => {
+      const data = items.data
+
+      // Tratando as datas
+      data.map((item) => {
+        item.created_at = new Date(item.created_at!).toLocaleDateString('pt-BR')
+      })
+
+      this.allKits = data;
+      this.kits = data;
+    })
+  }
+
+  search(e: Event): void {
+    const target = e.target as HTMLInputElement
+    const value = target.value
+
+    this.kits = this.allKits.filter((kit) => {
+      return kit.nome.toLowerCase().includes(value);
+    })
+  }
 }
